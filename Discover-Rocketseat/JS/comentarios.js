5825b8bb19468b974d3aa1810cf6813f4cef7273
william_gellesch@icloud.com <william_gellesch@icloud.com>
2023-05-26 14:13:27 -0300

    JS

---
+++ Discover-Rocketseat/JS/comentarios.js
@@ -0,0 +1,8 @@
+//Aqui eu tenho um comentario em linha
+
+/*
+Aqui eu tenho um comentario multi linhas ou bloco
+
+O código abaixo escreve alguma mensagem no devtools
+*/
+console.log("Bem vindos ao Starter")
