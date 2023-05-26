26f62d0b14e951aaed2a37815c0fc9a845ff51ca
william_gellesch@icloud.com <william_gellesch@icloud.com>
2023-05-26 15:52:11 -0300

    js

--- Discover-Rocketseat/JS/numeros.js
+++ Discover-Rocketseat/JS/numeros.js
@@ -7,3 +7,6 @@
         NaN // Not a Number
         Infinity // Infinito
 */
+console.log(33)
+console.log(12.5)  
+console.log(12.5 + 33)
---
+++ Discover-Rocketseat/JS/object.js
@@ -0,0 +1,13 @@
+/*
+    *Object
+        *Objeto
+        *Propriedade / Atributo
+        *Funcionalidade / Métodos
+
+    {propriedade: "valor"}
+    */
+   console.log({name: "William", 
+   idade: 34, 
+   andar: function(){console.log('andar')
+}
+})
