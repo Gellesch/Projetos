279d5d02c7e91e3c3c713ed4f0de87e626fdfb7a
william_gellesch@icloud.com <william_gellesch@icloud.com>
2023-05-30 16:40:06 -0300

    js

---
+++ Discover-Rocketseat/JS/array.js
@@ -0,0 +1,14 @@
+/*
+* array (Vetores)
+    * Uma lista
+    * Agrupamento de dado
+    * 
+    * ["William", 34]
+    * 
+*/
+console.log([
+    "Leite",
+    "Ovos",
+    2,
+    3,
+    ])
---
+++ Discover-Rocketseat/JS/variaveis.js
@@ -0,0 +1,21 @@
+/* # Variáveis 
+Nomes simbólicos para receber algum valor
+Atalhos de código
+Identificadores
+
+    3 palavras reservadas para criar uma variável
+        var
+        let
+        const 
+
+
+
+Tipos dinâmicos
+
+O JS  é uma linguagem fracamente tipada e dinâmica
+- Variáveis não precisam tem um tipo previamente definido
+- Podemos mudar o conteúdo da variável
+
+
+
+*/
