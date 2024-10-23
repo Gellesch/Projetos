02bde8f033fc812583c2b6bfce8b5f8001665cce
William Carniel Gellesch <102378890+Gellesch@users.noreply.github.com>
2024-10-23 16:41:19 -0300

    atualizações
    foguetes

--- RocketSeat/Intensivao stages 1-4/script.js
+++ RocketSeat/Intensivao stages 1-4/script.js
@@ -1,3 +1,19 @@
+window.addEventListener('load', () => {
+    const loadingScreen = document.getElementById('loading-screen');
+    const loadingFill = document.querySelector('.loading-fill');
+    
+    // Inicia a animação da barra de carregamento
+    loadingFill.style.width = '100%';
+    
+    // Define o tempo que corresponde à duração da animação da barra de progresso (5 segundos)
+    setTimeout(() => {
+      // Quando a barra de progresso atinge 100%, esconde a tela de loading
+      loadingScreen.classList.add('hidden');
+      document.body.classList.add('loaded'); // Mostra o conteúdo do site
+    }, 5000); // Tempo de 5 segundos para corresponder à animação da barra
+  });
+  
+
 const list = document.querySelectorAll('.list');
 
 function activeLink() {
